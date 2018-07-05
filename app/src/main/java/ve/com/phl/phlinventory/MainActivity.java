cf870c12dd189530ad2c2693c5dc8eeec2a67d79
GEORGEHOSS\Javier <georgehoss92@gmail.com>
2018-07-05 18:46:35 -0400

    Initial commit

---
+++ app/src/main/java/ve/com/phl/phlinventory/MainActivity.java
@@ -0,0 +1,19 @@
+package ve.com.phl.phlinventory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Fabric.with(this, new Crashlytics());
+       // getSupportFragmentManager().beginTransaction().replace(R.id.container, new MainFragment()).commit();
+    }
+}
